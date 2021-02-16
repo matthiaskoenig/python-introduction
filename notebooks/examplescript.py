f5a04ddab32990f9b6d34cfe211ca72d6b599d5e
Matthias Koenig <konigmatt@googlemail.com>
2021-02-16 22:15:55 +0100

    updated results

--- notebooks/examplescript.py
+++ notebooks/examplescript.py
@@ -1,7 +1,8 @@
 """External example script."""
+
 # file: examplescript.py
 # everything after '#' is a comment
- 
-x = 1.4
+
+x = 1.9
 y = 3.4
 print([x+y, x*y, x**y])
--- notebooks/my_functions.py
+++ notebooks/my_functions.py
@@ -8,3 +8,12 @@ def simple_sum(x, y):
 	"""
 	z = x + y
 	return z
+
+def simple_product(x, y):
+	"""Compute product of two numbers.
+
+	The function computes the product of two numbers
+	usage: z = simple_product(x,y)
+	"""
+	z = x * y
+	return z
